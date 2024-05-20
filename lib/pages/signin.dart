891511d1ec8b4c96eacfbd23cfe66930c395b638
manuel-spec <amaniase084@gmail.com>
2024-05-19 21:15:20 -0700

    input controllers for the textfields done

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SignInWidget extends StatelessWidget {
-  const SignInWidget({super.key});
+class SignInWidget extends StatefulWidget {
+  const SignInWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SignInWidget> createState() => _SignInWidgetState();
+}
+
+class _SignInWidgetState extends State<SignInWidget> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
