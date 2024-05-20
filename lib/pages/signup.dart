77946955ebb51d86610a2e65f148e442af7806c6
manuel-spec <amaniase084@gmail.com>
2024-05-19 21:25:15 -0700

    text field controllers Done

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SignUpWidget extends StatelessWidget {
-  const SignUpWidget({super.key});
+class SignUpWidget extends StatefulWidget {
+  const SignUpWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpWidget> createState() => _SignUpWidgetState();
+}
+
+class _SignUpWidgetState extends State<SignUpWidget> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
