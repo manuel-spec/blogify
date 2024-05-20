d7e25be0318c6c7bebe0e8daab72418f2585dab2
manuel-spec <amaniase084@gmail.com>
2024-05-19 21:13:41 -0700

    Signin placeholder

---
+++ lib/pages/signin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInWidget extends StatelessWidget {
+  const SignInWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
