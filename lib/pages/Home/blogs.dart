a735dbb180c9bd5d6ae163d8dcf1ed8c59862ef4
manuel-spec <amaniase084@gmail.com>
2024-06-07 16:53:33 -0700

    blog_post_detail_view_done

--- lib/pages/Home/blogs.dart
+++ lib/pages/Home/blogs.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:blogify/Models/userModel.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:ionicons/ionicons.dart';
@@ -61,90 +62,158 @@ class _BlogsWidgetState extends State<BlogsWidget> {
             itemCount: blogs.length,
             itemBuilder: (context, index) {
               final blog = blogs[index];
-              return Container(
-                // margin: EdgeInsets.symmetric(vertical: 40),
-                padding: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.black26))),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.symmetric(horizontal: 20),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.blueGrey,
-                              ),
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: SizedBox(
+                            height: 800,
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      blog.title,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      blog.description,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      blog.content,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    SizedBox(height: 20),
+                                    Text(
+                                      'Author: ${blog.user.name}',
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Username: @${blog.user.username}',
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            Container(
-                              child: Text(
-                                blog.user.name,
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(horizontal: 5),
-                              child: Text("@" + blog.user.username),
-                            )
-                          ],
+                          ),
                         ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 30),
-                                child: Text(
-                                  blog.title,
-                                  softWrap: true,
-                                  maxLines: null,
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  // margin: EdgeInsets.symmetric(vertical: 40),
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      border:
+                          Border(bottom: BorderSide(color: Colors.black26))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 20),
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.blueGrey,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 30, vertical: 6),
+                              Container(
                                 child: Text(
-                                  blog.description,
-                                  softWrap: true,
-                                  maxLines: null,
+                                  blog.user.name,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                            margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
-                            child: Icon(
-                              Ionicons.heart_outline,
-                              color: Color.fromARGB(255, 220, 220, 220),
-                            )),
-                        Container(
-                            margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
-                            child: Icon(
-                              Icons.message_outlined,
-                              color: Color.fromARGB(255, 220, 220, 220),
-                            )),
-                        Container(
-                            margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
-                            child: Icon(
-                              Icons.share,
-                              color: Color.fromARGB(255, 220, 220, 220),
-                            )),
-                      ],
-                    )
-                  ],
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 5),
+                                child: Text("@" + blog.user.username),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30),
+                                  child: Text(
+                                    blog.title,
+                                    softWrap: true,
+                                    maxLines: null,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30, vertical: 6),
+                                  child: Text(
+                                    blog.description,
+                                    softWrap: true,
+                                    maxLines: null,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                              margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
+                              child: Icon(
+                                Ionicons.heart_outline,
+                                color: Color.fromARGB(255, 220, 220, 220),
+                              )),
+                          Container(
+                              margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
+                              child: Icon(
+                                Icons.message_outlined,
+                                color: Color.fromARGB(255, 220, 220, 220),
+                              )),
+                          Container(
+                              margin: const EdgeInsets.fromLTRB(50, 10, 0, 0),
+                              child: Icon(
+                                Icons.share,
+                                color: Color.fromARGB(255, 220, 220, 220),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             },
