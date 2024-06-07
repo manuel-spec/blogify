a1d26f8dd0b3731aeac5a610718735fd9e8552d4
manuel-spec <amaniase084@gmail.com>
2024-06-07 15:37:26 -0700

    profile_ui_update

--- lib/pages/Profile/profile.dart
+++ lib/pages/Profile/profile.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:blogify/pages/Profile/profile_info.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -26,113 +27,32 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  String name = "";
-  String username = "";
-  String bio = "";
-
-  void _logout() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    preferences.clear();
-    context.go('/profile');
-  }
-
-  void _fetchUserInformation() async {
-    // Fetch user information from the server http://192.168.201.112:9000/api/users/:id
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String token = prefs.getString("token")!;
-    String id = prefs.getInt('id').toString();
-
-    var url = "http://192.168.201.112:9000/api/users/$id";
-    final response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> res = json.decode(response.body);
-      setState(() {
-        name = res['user']['name'];
-        username = res['user']['username'];
-      });
-
-      print(res['user']);
-    } else {
-      print(response.body);
-      throw Exception('Failed to load blogs');
-    }
-  }
-
-  void initState() {
-    super.initState();
-    _fetchUserInformation();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: CircleAvatar(
-                    radius: 60,
-                    backgroundImage: NetworkImage(
-                        'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'), // Replace with your profile image URL
-                  ),
-                ),
-                SizedBox(height: 16),
-                Center(
-                  child: Text(
-                    name,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      // color: Colors.white,
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    '@$username',
-                    style: TextStyle(
-                      fontSize: 18,
-                      // color: Colors.white70,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Center(
-                  child: TextButton(
-                    onPressed: _logout,
-                    child: Text('logout'),
-                    style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.white),
-                        backgroundColor: MaterialStateProperty.all(Colors.red),
-                        padding: MaterialStateProperty.all(EdgeInsets.all(16)),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)))),
-                  ),
-                )
-              ],
-            ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          toolbarHeight: 0, // Remove the default AppBar height
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "Profile",
+              ),
+              Tab(
+                text: "Update",
+              ),
+            ],
           ),
+        ),
+        body: const TabBarView(
+          children: [
+            ProfileInfo(),
+            Text("Profile Update"),
+          ],
         ),
       ),
     );
---
+++ lib/pages/Profile/profile_update.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileUpdate extends StatefulWidget {
+  const ProfileUpdate({super.key});
+
+  @override
+  State<ProfileUpdate> createState() => _ProfileUpdateState();
+}
+
+class _ProfileUpdateState extends State<ProfileUpdate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
